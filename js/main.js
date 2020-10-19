d62992db7d52502bea1a25c1684fce34cdedbd90
mohamed37 <mohamedelmeleeh@gmail.com>
2020-10-19 14:49:50 -0700

    del

--- js/main.js
+++
@@ -1,7 +0,0 @@
-
-$(Window).on("scroll", function () {
-    $(".navbar").css("backgroundColor", "#03043226");
-    if ($(window).scrollTop() === 0) {
-        $(".navbar").css("backgroundColor", "#fff");
-    }
-});
