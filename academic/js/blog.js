8662b05b2a31cf4a46bc12122ee86dfe0499e936
Doaa-Takriti <doaatakriti@gmail.com>
2020-10-28 22:49:09 -0600

    adderror

---
+++ academic/js/blog.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+$("#for-button1").click(function (){
+	$("#for-reply1").toggle();
+});
+$("#for-button2").click(function (){
+	$("#for-reply2").toggle();
+});
+$("#for-button3").click(function (){
+	$("#for-reply3").toggle();
+});
+	});
